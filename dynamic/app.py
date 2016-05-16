6fe12465e96913f91d47367922a5781fa8442981
fans656 <fans656@yahoo.com>
2016-05-16 13:32:58 +0800

    ub

---
+++ dynamic/app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from datetime import datetime
+import subprocess
+import os
+
+app = Flask(__name__)
+app.debug = True
+
+@app.route('/pull')
+def pull():
+    try:
+        path = os.path.dirname(__file__)
+        os.chdir(path)
+        output = subprocess.check_output('git status', shell=True)
+        s = 'pulled at {}'.format(datetime.now())
+        s += '<pre>{}</pre>'.format(output)
+    except Exception as e:
+        return 'exception:\n' + str(e)
+    return s
+
+if __name__ == '__main__':
+    app.run(debug=True, listen=('', 6560))
---
+++ dynamic/main.wsgi
@@ -0,0 +1,7 @@
+import sys
+sys.path.insert(0, '/var/www/fans656_blog/dynamic')
+sys.path.insert(1, '/var/www/fans656_blog/')
+sys.path.insert(2, '/usr/local/lib/python2.7/dist-packages')
+from app import app
+from werkzeug.debug import DebuggedApplication
+application = DebuggedApplication(app, True)
